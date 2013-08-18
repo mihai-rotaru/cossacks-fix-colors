b81b5f1a32a8ad5a0897be7b49bf6d2a2b43e1cd
Mihai Rotaru <mihai_rotaru@lavabit.com>
2013-08-18 12:37:45 +0100

    error if not admin

--- cossacks-fix-colors-mod1.bat
+++ cossacks-fix-colors-mod1.bat
@@ -1,6 +1,23 @@
 :: Start Cossacks with Mod 1 enabled and colors fixed
 
 @ECHO OFF
+
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    GOTO ADMIN_OK
+) else (
+    GOTO NO_ADMIN
+)
+
+:NO_ADMIN
+echo Error: file run without administartor permissions.
+echo Please right-click on the script and select "Run as administrator"
+PAUSE
+exit
+
+:ADMIN_OK
+echo Success: Administrative permissions confirmed.
+
 set COSSACKS_DIR=%~dp0
 
 taskkill /f /im explorer.exe
